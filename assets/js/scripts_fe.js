4e628d79fd6436ff2561da7cb464445d7d496a88
Victor <vkosmenko@gmail.com>
2018-08-04 07:21:36 +0300

    Cookies message local storage functionality

--- assets/js/scripts_fe.js
+++ assets/js/scripts_fe.js
@@ -3,6 +3,17 @@ jQuery(function ($) {
 
         console.log('FE Ready');
         
+        // Show popup cookies message if needed
+        var ShowVKCookiesMessage = localStorage.getItem("show-vk-cookies-message");
+        if(ShowVKCookiesMessage !== 'false'){
+            $('#vk-cookies-wrapper').show();
+        }
+        
+        // Cookies message buttons click
+        $('#vk-cookies-button-close, #vk-cookies-button-accept').click(function(){
+            localStorage.setItem('show-vk-cookies-message', false);
+            $('#vk-cookies-wrapper').hide();
+        });
         
     });
 });
